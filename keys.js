702c5d28093fee3d4f1e462ac95d5332b70d499e
Rikki Honnold <rikki@rikkis-mbp.lan>
2018-04-16 22:51:42 -0500

    found curly brace

--- keys.js
+++ keys.js
@@ -1,60 +1,14 @@
-
 require('dotenv').config()
-var Twitter = require('twitter');
- 
-var client = new Twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
-});
- 
-var params = {screen_name: 'nodejs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  }
-});
+console.log('this is loaded');
 
-//request  
+exports.twitter = {
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+};
 
-//https://www.npmjs.com/package/node-spotify-api
-
-var Spotify = require('node-spotify-api');
- 
-var spotify = new Spotify({
-  id: <a9074fb337aa45e4b29d261db998b8a8>,
-  secret: 53d3955d713345f6981ecbbe4d472320
-});
- 
-spotify
-  .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
-  .then(function(data) {
-    console.log(data); 
-  })
-  .catch(function(err) {
-    console.error('Error occurred: ' + err); 
-  });
-
-  //search
-  var Spotify = require('node-spotify-api');
- 
- var spotify = new Spotify({
-   id: <your spotify client id>,
-   secret: <your spotify client secret>
- });
-  
- spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-   if (err) {
-     return console.log('Error occurred: ' + err);
-   }
-  
- console.log(data); 
- });
-
- var request = require('request');
-request('http://www.google.com', function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-});
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
--- liri.js
+++ liri.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
-require("keys.js");
+require("/Users/rikki/Desktop/liri-node-app/keys.js");
 let fs = require("fs");
-let action = process.argv;
+let action = process.argv[2];
 var keys = new Keys(keys);
 let Twitter = require('twitter');
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
-var Spotify = require('node-spotify-api')
+var Spotify = require('node-spotify-api');
+
 
 var client = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -14,6 +15,7 @@ var client = new Twitter({
     access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
   });
+  console.log('this is loaded');
   //Make it so liri.js can take in one of the following commands:
 //* `my-tweets`
 //* `spotify-this-song`
@@ -21,7 +23,7 @@ var client = new Twitter({
 //* `do-what-it-says`
 
 switch (action) {
-    case "my-tweets"":
+    case "my-tweets":
       tweets();
       break;
     
@@ -37,28 +39,23 @@ switch (action) {
       what-it();
       break;
     }
-    
-    function tweets() {
-        client.get('search/tweets', {q: 'node.js'}, function(error, tweets, response) {
-            console.log(tweets);
-         });
-      
+            
    function tweets() {
-       var params = {screen_name: 'nodejs'};
-   client.get('statuses/user_timeline', params, function(error, tweets, response) {
-     if (!error) {
+       var params = {screen_name: 'spoonieluvmn'};
+       client.get('statuses/user_timeline', params, function(error, tweets, response) {
+       if (!error) {
        console.log(tweets);
      }
    });}
 
    function spotify() {
-    .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+    request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
     .then(function(data) {
       console.log(data); 
     })
     .catch(function(err) {
       console.error('Error occurred: ' + err); 
-    spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data, body) {
+    spotify.search({type: 'track', query: 'All the Small Things' }), function(err, data, body) {
         if (err) {
           return console.log('Error occurred: ' + err);
         }
@@ -67,16 +64,16 @@ switch (action) {
       console.log("This song is called : " + JSON.parse(body).tracks);
       console.log("Here's a link to the song " + JSON.parse(body).href);
       console.log("This song appears on this albumn " + JSON.parse(body).name);
-      console.log("Artists's name is  " + JSON.parse(body).artists);;  
-      });
-   }
+      console.log("Artists's name is  " + JSON.parse(body).artists); 
+    }
+  });}
+  
    function movie() {
        //f the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
     //If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
     //It's on Netflix!
-    request("http://www.omdbapi.com/?t="+ movieName + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
+    request("http://www.omdbapi.com/?t="+ movieName + "&y=&plot=short&apikey=trilogy"), function(error, response, body) {
         if (!error && response.statusCode === 200) {
-
             // Parse the body of the site and recover just the imdbRating
             // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
             console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
@@ -84,7 +81,5 @@ switch (action) {
             console.log("The movie's on IMDB here: " + JSON.parse(body).title);
             console.log("The movie's on IMDB here: " + JSON.parse(body).imdbID);
           }
-    }
-
-
-   function what-it() {}
+        }
+      }
